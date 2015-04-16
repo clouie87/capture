f31825420d4547ac6f6c52a4e6e348489a7de4b0
clouie87 <carol.louie@gmail.com>
2015-04-16 10:42:40 -0300

    getting login and signup forms ready

--- www/js/controllers/login.js
+++ www/js/controllers/login.js
@@ -1,6 +1,22 @@
 'use strict';
 
-app.controller('LoginController', function($scope){
-  console.log('got to the PhotoContoller linked');
+app.controller('LoginController', function($scope, $ionicPopup){
+  console.log('got to the LoginController linked');
+
+  $scope.showLogin = function() {
+    console.log('open login modal');
+    var myLogin = $ionicPopup.show({
+      templateUrl: 'templates/partials/login.html',
+      title: 'Login'
+    })
+  }
+
+  $scope.showSignup = function() {
+    console.log('open signup modal');
+    var mySignup = $ionicPopup.show({
+      templateUrl: 'templates/partials/signup.html',
+      title: 'Signup'
+    })
+  }
 
 });
